c60a5addd31b3adac935047c1817e8cc6d4c7908
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-10-05 14:52:56 +0600

    remove encoded

--- encoded-classname-next-app/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-      <div className='py-5 bg-teal-700'>
-        <h1 className='text-2xl font-mono uppercase text-center font-bold text-white'>Encoded class name</h1>
-      </div>
-    )
-  } 
-  
-  export default Header
