14363940e77bbfa9b07d736288aa292961ee14f6
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-10-25 16:13:28 +0600

    pdf added

--- random-app/routes/web.php
+++ random-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('generate-pdf', [App\Http\Controllers\PDFController::class, 'generatePDF'])->name('generate-pdf');
+
