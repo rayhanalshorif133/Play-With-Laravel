0efac1b597cf76ab42f8340c63a927d6bd97d830
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-12-26 17:25:49 +0600

    wokring on it

---
+++ laravel/vue3/app_1/resources/js/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="bg-gray-300">{{ title }}</h1>
+    <br/>
+    <button @click="handleClick" class="py-2 px-4 border border-green-600  text-green-600 hover:bg-green-600 hover:text-white">Click Me</button>
+</template>
+<script>
+export default {
+    setup: () => ({
+        title: 'How To Install Vue 3 in Laravel 8 From Scratch',
+        handleClick: () => {
+            alert('Vue 3 is working!')
+        }
+    })
+}
+</script>
