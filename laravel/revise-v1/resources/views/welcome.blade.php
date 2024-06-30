046a9db5174ac60ca56185f02bf44475cb566d7a
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-06-30 12:30:00 +0600

    make it laravel

---
+++ laravel/revise-v1/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+
+
+</head>
+
+<body>
+    <ul>
+        <li>plugin-browser-sync install</li>
+        <li>install talwind</li>
+        <li>header</li>
+        <li>body working card</li>
+        <li></li>
+    </ul>
+
+</body>
+
+</html>
